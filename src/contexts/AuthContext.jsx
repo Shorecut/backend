6802f21326cfa486a7f70e8021ddf1d913683965
Shorecut <chermanderco@gmail.com>
2023-08-02 10:30:55 +0600

    qwe

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useContext } from "react";
+
+const authContext = createContext();
+
+export function useAuthContext() {
+  return useContext(authContext);
+}
+
+const AuthContext = ({ children }) => {
+  const value = {};
+  return <authContext.Provider value={value}>{children}</authContext.Provider>;
+};
+
+export default AuthContext;
